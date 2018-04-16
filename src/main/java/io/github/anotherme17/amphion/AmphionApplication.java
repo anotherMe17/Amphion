37322aba94e29314db3adce821afe836b2440ff4
lirenhao <anotherme@aliyun.com>
2018-04-16 23:54:06 +0800

    first init

---
+++ src/main/java/io/github/anotherme17/amphion/AmphionApplication.java
@@ -0,0 +1,12 @@
+package io.github.anotherme17.amphion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmphionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmphionApplication.class, args);
+	}
+}
