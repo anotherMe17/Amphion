291ab8097fd918f4b966df0a87194e5876f4afd5
lirenhao <anotherme@aliyun.com>
2018-04-17 18:12:29 +0800

    :arrow_down: Add MyBatis-SpringBoot Maven
    - use mybatis to develop mysql

--- src/main/java/io/github/anotherme17/amphion/AmphionApplication.java
+++ src/main/java/io/github/anotherme17/amphion/AmphionApplication.java
@@ -1,9 +1,11 @@
 package io.github.anotherme17.amphion;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.github.anotherme17.amphion.mapper")
 public class AmphionApplication {
 
 	public static void main(String[] args) {
